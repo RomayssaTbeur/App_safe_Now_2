60cb717535d3298113cb93cbb71233831469b36c
RomayssaTbeur <tbeurromayssa1@gmail.com>
2026-04-11 16:14:57 +0100

    change themes and vr-simulation-activity

--- app/src/main/java/com/example/safe_now_2/controller/MainActivity.java
+++ app/src/main/java/com/example/safe_now_2/controller/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.safe_now_2;
+package com.example.safe_now_2.controller;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.safe_now_2.R;
 
 public class MainActivity extends AppCompatActivity {
 
